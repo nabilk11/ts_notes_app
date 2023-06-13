3fa73c5d59c3efc00ed1f77eb3cca1ea05022a5f
nabilk11 <khan.nabiln11.com>
2023-06-13 02:15:21 -0400

    added react router elements and basic page routing for Home and NewNote

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./pages/NewNote";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>Notes App</h1>
-    </div>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<NewNote />} />
+        {/* nested routes */}
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </Container>
   );
 }
 

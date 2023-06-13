dbc575f5ed59409a8c6d1f784316d6f8c97f7de1
nabilk11 <khan.nabiln11.com>
2023-06-13 01:34:33 -0400

    first commit - removed unwanted files and imports and added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes App
+- A comprehensive note taking application that supports Markdown. Built using React & Typescript through Vite.
+
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Notes App</h1>
+    </div>
+  );
+}
+
+export default App;

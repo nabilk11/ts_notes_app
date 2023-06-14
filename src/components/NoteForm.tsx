ed42c2f5284e9acf9ea409663e6af3b9bd38d5e5
nabilk11 <khan.nabiln11.com>
2023-06-14 00:37:56 -0400

    created NewNote page with new NoteForm component. Installed react-select to create multi select form control input

---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
+
+const NoteForm = () => {
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as={"textarea"} rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button variant="primary" type="submit">
+            Save
+          </Button>
+          <Link to={".."}>
+            <Button variant="secondary" type="button">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+};
+
+export default NoteForm;
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NoteForm from "../components/NoteForm";
+
+const NewNote = () => {
+  return (
+    <>
+      <h1 className="mb-4">NewNote</h1>
+      <NoteForm />
+    </>
+  );
+};
+
+export default NewNote;

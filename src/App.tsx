57b11370f4b0b2baffb54718b141944220bc5705
nabilk11 <khan.nabiln11.com>
2023-06-15 03:16:15 -0400

    added types for note, notedata, rawnotes, rawnotedata. and tags. added notes and tags, and setNotes, and setTags to custom useLocalStorage hook - created useLocalStorage hook in src/hooks folder.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,37 @@ import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./pages/NewNote";
 import Home from "./pages/Home";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
+// adds id to existing NoteData
+export type Note = {
+  id: string;
+} & NoteData;
+
+// types for NoteData
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type RawNote = {
+  id: string;
+};
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
   return (
     <Container className="my-4">
       <Routes>
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+// useLocalStorage Hook
+// checks if value exists yet, and when it does - update it 
+
+import { useEffect, useState } from "react"
+
+export const useLocalStorage = <T>(key: string, initialValue: T | (()=> T)) => {
+    const [value, setValue] = useState<T>(()=> {
+      const jsonValue = localStorage.getItem(key)
+      if (jsonValue == null){
+        if (typeof initialValue === "function"){
+            return (initialValue as () => T)()
+        }
+      } else {
+        return JSON.parse(jsonValue)
+      }
+    })
+
+    // useEffect to save values in localstorage when value or key changes
+    useEffect(()=> {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [value, key])
+    return [value, setValue] as [T, typeof setValue]
+}

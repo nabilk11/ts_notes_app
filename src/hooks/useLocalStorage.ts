f3b7577bee548d8a93fbd5955629276211385246
nabilk11 <khan.nabiln11.com>
2023-07-23 17:34:04 -0400

    added missing else statement for initial local storage values

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,15 +3,14 @@
 
 import { useEffect, useState } from "react";
 
-export const useLocalStorage = <T>(
-  key: string,
-  initialValue: T | (() => T)
-) => {
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
         return (initialValue as () => T)();
+      } else {
+        return initialValue;
       }
     } else {
       return JSON.parse(jsonValue);
@@ -22,5 +21,6 @@ export const useLocalStorage = <T>(
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
+
   return [value, setValue] as [T, typeof setValue];
-};
+}

97d7f0fec62bb183dcf3663afca9979b7b0f58db
nabilk11 <khan.nabiln11.com>
2023-07-23 17:36:37 -0400

    added tags to NewNoteProps and passed down to NoteForm

--- src/pages/NewNote.tsx
+++ src/pages/NewNote.tsx
@@ -1,15 +1,21 @@
 import NoteForm from "../components/NoteForm";
-import { NoteData } from "../App";
+import { NoteData, Tag } from "../App";
 
 type NewNoteProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-const NewNote = ({ onSubmit }: NewNoteProps) => {
+const NewNote = ({ onSubmit, onAddTag, availableTags }: NewNoteProps) => {
   return (
     <>
       <h1 className="mb-4">NewNote</h1>
-      <NoteForm onSubmit={onSubmit} />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 };
